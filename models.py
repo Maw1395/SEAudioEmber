4bd9ce68505c8c63de16241329e54115a18d2965
Robert Gonzalez <labratjazzcat@users.noreply.github.com>
2017-07-24 21:11:35 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class songs(db.Model):
-        SongID = db.Column(db.Integer, nullable= False, primary_key=True)
+        SongID = db.Column(db.Integer, primary_key=True)
         ArtistID = db.Column(db.Integer)
         Artist = db.Column(db.String(120))
         Title = db.Column(db.String(120))
@@ -19,13 +19,13 @@ class artist_points(db.Model):
             return "%s, %d\n" %(self.Artist, self.Points)
 
 class graphs(db.Model):
-        SongID = db.Column(db.String(120), db.ForeignKey("songs.SongID"), nullable = False, primary_key = True)
+        SongID = db.Column(db.String(120), primary_key = True)
         URL = db.Column(db.String(120), primary_key=True)
         def __repr__self(self):
             return "%s, %s\n" %(self.SongID, self.URL)
 
 class song_points(db.Model):
-        SongID = db.Column(db.Integer,db.ForeignKey("songs.SongID"), nullable = False, primary_key = True)
+        SongID = db.Column(db.Integer, primary_key = True)
         ArtistID = db.Column(db.Integer)
-        Genre = db.Column(db.String(25), db.ForeignKey("points.Genre"), nullable = False, primary_key = True)
+        Genre = db.Column(db.String(25), primary_key = True)
         Points = db.Column(db.Integer)

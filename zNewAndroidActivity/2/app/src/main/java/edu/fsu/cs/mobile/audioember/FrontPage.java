d0ee9a0a019d7945d5d6880ccadc1fdedb2ac62f
Michael Woodham <maw13r@my.fsu.edu>
2017-07-28 15:28:59 -0400

    New Xmls

---
+++ zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/FrontPage.java
@@ -0,0 +1,45 @@
+package edu.fsu.cs.mobile.audioember;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class FrontPage extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.front_page_song_selector:
+                    mTextMessage.setText(R.string.top_songs);
+                    return true;
+                case R.id.front_page_artist_selector:
+                    mTextMessage.setText(R.string.top_artists);
+                    return true;
+                case R.id.front_page_genre_selector:
+                    mTextMessage.setText(R.string.genre_list);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}

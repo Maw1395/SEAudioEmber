3909922ff25a2bb06ef792e6f303c30e8dbfa295
dmx36502@yahoo.com <dmx36502@yahoo.com>
2017-07-31 17:41:16 -0400

    d

--- zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/FrontPage.java
+++ zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/FrontPage.java
@@ -3,11 +3,11 @@ package edu.fsu.cs.mobile.audioember;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class FrontPage extends AppCompatActivity {
+public class FrontPage extends FragmentActivity {
 
     private TextView mTextMessage;
 
@@ -37,8 +37,32 @@ public class FrontPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
 
+        //For TopSong, TopArtist, Genre fragments
+        setContentView(R.layout.fragment);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            TopSongListFragment songFragment = new TopSongListFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            //songtFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            //getSupportFragmentManager().beginTransaction().add
+        }
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 }

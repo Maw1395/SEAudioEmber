0acf3791f54834b4c7321330575abce228add7cb
dmx36502@yahoo.com <dmx36502@yahoo.com>
2017-07-31 17:41:28 -0400

    d

--- zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/GenreListFragment.java
+++ zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/GenreListFragment.java
@@ -1,8 +1,10 @@
 package edu.fsu.cs.mobile.audioember;
 
+import android.support.v4.app.ListFragment;
+
 /**
  * Created by woody on 7/28/17.
  */
 
-public class GenreListFragment {
+public class GenreListFragment extends ListFragment {
 }

8d0e7a457ab1cd3a4ca72a4961444be87f43ce09
Michael Woodham <maw13r@my.fsu.edu>
2017-08-02 15:08:55 -0400

    Fragment issues

--- zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/SongVideoGraphPage.java
+++ zNewAndroidActivity/2/app/src/main/java/edu/fsu/cs/mobile/audioember/SongVideoGraphPage.java
@@ -3,9 +3,10 @@ package edu.fsu.cs.mobile.audioember;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,20 +18,23 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static edu.fsu.cs.mobile.audioember.R.id.container;
 
 /**
  * Created by woody on 7/30/17.
  */
 
 public class SongVideoGraphPage extends Fragment{
-    // For the video
-    //WebChromeClient mWebChromeClient = null;
+
     WebChromeClient mWebChromeClient = null;
     WebChromeClient chartChromeClient = null;
     View mCustomView;
@@ -38,17 +42,23 @@ public class SongVideoGraphPage extends Fragment{
     FrameLayout mCustomViewContainer;
     WebChromeClient.CustomViewCallback mCustomViewCallback;
 
-    WebView myWebView;
-    WebView chart;
+    WebView YouTubeVideo;
+    WebView chart0;
+    WebView chart1;
+    WebView chart2;
+    WebView chart3;
+    WebView chart4;
+    WebView chart5;
     WebView[] webViewArray = new WebView[7];
+    WebChromeClient[] webChromeArray = new WebChromeClient[7];
     String html;
     String[] SongHtml = {};
 
 
     @Override
-        public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.song_result);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dyanmic_graph, container, false);
         ScrollView sv = new ScrollView(getContext());
         sv.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         final LinearLayout layout = new LinearLayout(getContext());
@@ -58,48 +68,54 @@ public class SongVideoGraphPage extends Fragment{
         layout.setLayoutParams(params);
         layout.setOrientation(LinearLayout.VERTICAL);
         sv.addView(layout);
-        getActivity().setContentView(sv);
+
         final LinearLayout.LayoutParams webViewParams= new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        // Sets the text for the action bar title
-        /*getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.actionbar_song_result);*/
 
-        // Chart webview
-        //mWebView = (WebView) findViewById(R.id.video_webview);
-        //mWebChromeClient = new WebChromeClient();
 
-        // For chart
-        final String songName = "SugarDaddy";
-        final String artistName = "PajamaBand";
-        final String url = "https://www.youtube.com/results?search_query=" + songName + " by " + artistName;
+        Bundle b = getArguments();
+        String songName;
+        String artistName;
+        ArrayList<String> GraphUrls= new ArrayList<String>();
+        if(b!=null)
+        {
+            songName = b.getString("Title");
+            artistName= b.getString("Artist");
+            GraphUrls = b.getStringArrayList("GraphUrls");
+        }
+        else
+        {
+            songName = "Closer";
+            artistName= "ChainSmokers Featuring Halsey";
+            GraphUrls.add("<iframe id=\"igraph\" scrolling=\"no\" style=\"border:none;\" seamless=\"seamless\" src=\"https://plot.ly/~audioembers/20335.embed\" height=\"525\" width=\"100%\"></iframe>");
+            GraphUrls.add("<iframe id=\"igraph\" scrolling=\"no\" style=\"border:none;\" seamless=\"seamless\" src=\"https://plot.ly/~audioembers/20337.embed\" height=\"525\" width=\"100%\"></iframe>");
+            GraphUrls.add("<iframe id=\"igraph\" scrolling=\"no\" style=\"border:none;\" seamless=\"seamless\" src=\"https://plot.ly/~audioembers/20339.embed\" height=\"525\" width=\"100%\"></iframe>");
+
+
+        }
+
+
+        final String youtubeSearchQuery = "https://www.youtube.com/results?search_query=" + songName + " by " + artistName;
         Thread ConnectionThread = new Thread() {
             public void run()
             {
                 Document jSoupDocument = null;
                 try {
-                    jSoupDocument = Jsoup.connect(url).get();
+                    jSoupDocument = Jsoup.connect(youtubeSearchQuery).get();
                     String youtubeEmbedLink=jSoupDocument.select("a.yt-uix-tile-link").first().attr("abs:href");
 
 
 
                     final String url="<iframe width=\"100%\" height=\"355\" src=\"https://www.youtube.com/embed/"+ youtubeEmbedLink.substring(youtubeEmbedLink.indexOf("?v=") +3) +"\" frameborder=\"0\" allowfullscreen==\"allowfullscreen\"></iframe>";
-                    // Element link = jSoupDocument.select("a[href=/watch]").first();
-                    //String AttrEmbed = link.attr("href");
-                    //String url = "https://www.youtube.com/embed/" + testhref.substring(testhref.indexOf(".com/")) +"?autoplay=1";
                     Log.d("UrlBeingParsed", url);
                     if(youtubeEmbedLink!=null)
                     {
                         new Handler(Looper.getMainLooper()).post(new Runnable() {
                             @Override
                             public void run() {
-                                myWebView.loadData(url, "text/html", "UTF-8");
-                                webViewArray[0]=myWebView;
-                                chart.loadData(html, "text/html", null);
-                                webViewArray[1]=chart;
+                                webViewArray[0].loadData(url, "text/html", "UTF-8");
+                                webViewArray[1].loadData(html, "text/html", null);
                                 layout.addView(webViewArray[1], webViewParams);
                                 layout.addView(webViewArray[0], webViewParams);
-                                //chart.loadData(url,  "text/html", null);
-                                //layout.addView(chart, webViewParams);
                             }
                         });
 
@@ -113,37 +129,26 @@ public class SongVideoGraphPage extends Fragment{
         };
 
 
-
-        chart = new WebView(getContext());
-        chartChromeClient = new WebChromeClient();
-        chart.setWebChromeClient(chartChromeClient);
-        chart.setWebViewClient(new WebViewClient()
+        for (int i=0; i<webViewArray.length; i++)
         {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            webViewArray[i] = new WebView(getContext());
+            webChromeArray[i] = new WebChromeClient();
+            webViewArray[i].setWebChromeClient(webChromeArray[i]);
+            webViewArray[i].setWebViewClient(new WebViewClient()
             {
-                return false;
-            }
-        });
-        WebSettings chartSettings = chart.getSettings();
-        chartSettings.setJavaScriptEnabled(true);
-        html = "<iframe id=\"igraph\" scrolling=\"no\" style=\"border:none;\" seamless=\"seamless\" src=\"https://plot.ly/~audioembers/21985.embed\" height=\"355\" width=\"100%\"></iframe>";
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url)
+                {
+                    return false;
+                }
+            });
+            WebSettings webset = webViewArray[i].getSettings();
+            webset.setJavaScriptEnabled(true);
+        }
 
-        // For Video
-        myWebView = new WebView(getContext());
-        mWebChromeClient = new WebChromeClient();
-        myWebView.setWebChromeClient(mWebChromeClient);
-        myWebView.setWebViewClient(new WebViewClient()
-        {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        // myWebView.loadUrl("https://www.youtube.com/watch?v=YQHsXMglC9A");
+
         ConnectionThread.start();
+        return rootView;
     }
 
 }

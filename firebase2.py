293d95eeb7a2decac8a8d2756986d54723c6f31b
bencookfsu@users.noreply.github.com <bcookmail@gmail.com>
2017-11-02 13:57:54 -0400

    New firebase2.py for updating firebase from database. Continued to bring code up to PEP8 standard.

--- firebase2.py
+++ firebase2.py
@@ -1,26 +1,27 @@
 from firebase import firebase
 from app import db, models
-from datetime import datetime
+import json
+import unicodedata
 
 firebase = firebase.FirebaseApplication('https://seaudioember.firebaseio.com/', None)
 
 SONG_MAX = db.session.query(models.songs).order_by(models.songs.SongID.desc()).first().SongID
-GenreList=['hot-100','country-songs', 'rock-songs','r-b-hip-hop-songs','dance-club-play-songs','pop-songs']
-for i in range(SONG_MAX):
+GenreList = ['hot-100', 'country-songs', 'rock-songs', 'r-b-hip-hop-songs', 'dance-club-play-songs', 'pop-songs']
+for i in range(54, SONG_MAX):
     print (i)
     songEntry = i
     for j in range(len(GenreList)):
-        IndividualSongList = db.session.query(models.songs).filter(models.songs.SongID==songEntry,
-                models.songs.Genre==GenreList[j])
-        if IndividualSongList!=None:
+        IndividualSongList = db.session.query(models.songs).filter(models.songs.SongID == songEntry,
+                                                                   models.songs.Genre == GenreList[j])
+        if IndividualSongList is not None:
             for IndividualSongEntry in IndividualSongList:
-                Date= IndividualSongEntry.Date
+                Date = IndividualSongEntry.Date
                 Points = IndividualSongEntry.Points
-                Artist = IndividualSongEntry.Artist
-                Title = IndividualSongEntry.Title
+                Artist = unicodedata.normalize('NFKD', IndividualSongEntry.Artist).encode('ascii', 'ignore')
+                Title = unicodedata.normalize('NFKD', IndividualSongEntry.Title).encode('ascii', 'ignore')
                 SongID = IndividualSongEntry.SongID
-                path = "SongByDay/" + str(SongID) + '/' +GenreList[j] +'/' + Date.strftime('%Y-%m-%d') +'/'
-                firebase.post(path, {str(Title), str(Artist), str(Points)} )
+                path = "SongByDay/" + str(SongID) + '/' + GenreList[j] + '/' + Date.strftime('%Y-%m-%d') + '/'
 
-
-
+                data = {'Points': Points, 'Artist': Artist, 'Title': Title}
+                data = json.dumps(data)
+                firebase.post(path, data)
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class songs(db.Model):
         SongID = db.Column(db.Integer, primary_key=True)
         ArtistID = db.Column(db.Integer)
@@ -8,26 +9,32 @@ class songs(db.Model):
         Genre = db.Column(db.String(25), primary_key=True)
         Points = db.Column(db.Integer)
         Date = db.Column(db.Date, primary_key=True)
+
         def __repr__self(self):
-                return "%d, %s, %d\n" %(self.SongID, self.Genre, self.Points)
+            return "%d, %s, %d\n" %(self.SongID, self.Genre, self.Points)
+
 
 class artist_points(db.Model):
         Artist = db.Column(db.String(120), primary_key=True)
         ArtistID = db.Column(db.Integer)
         Genre = db.Column(db.String(25), primary_key=True)
         Points = db.Column(db.Integer)
+
         def __repr__self(self):
             return "%s, %d\n" %(self.Artist, self.Points)
 
+
 class graphs(db.Model):
-        SongID = db.Column(db.String(120), primary_key = True)
+        SongID = db.Column(db.String(120), primary_key=True)
         URL = db.Column(db.String(120), primary_key=True)
+
         def __repr__self(self):
-            return "%s, %s\n" %(self.SongID, self.URL)
+            return "%s, %s\n" % (self.SongID, self.URL)
+
 
 class song_points(db.Model):
-        SongID = db.Column(db.Integer, primary_key = True)
+        SongID = db.Column(db.Integer, primary_key=True)
         ArtistID = db.Column(db.Integer)
         Artist = db.Column(db.String(120))
-        Genre = db.Column(db.String(25), primary_key = True)
+        Genre = db.Column(db.String(25), primary_key=True)
         Points = db.Column(db.Integer)
